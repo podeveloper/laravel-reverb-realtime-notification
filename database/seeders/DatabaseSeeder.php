404163e38df059b14dfd99a429f308f2c542a449
Yasin Korkmaz <yasinkorkmaz.dev@gmail.com>
2024-07-18 11:07:39 +0300

    Seeders and View

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'admin@admin.com',
         ]);
+
+        User::factory()
+            ->count(1000)
+            ->create();
     }
 }
